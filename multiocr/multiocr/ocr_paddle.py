82f48cbff1287d1bad0969a6ad6e56f61c71d802
Aravindh <32878238+s-aravindh@users.noreply.github.com>
2023-01-05 21:52:15 +0800

    paddle init

---
+++ multiocr/multiocr/ocr_paddle.py
@@ -0,0 +1,14 @@
+from base_class import OCR
+
+
+
+class PaddleOCR(ABC):
+
+    def image_to_str(self):
+        pass
+
+    def image_to_data(self):
+        pass
+
+    def image_to_json(self):
+        pass
