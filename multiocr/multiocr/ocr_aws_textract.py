9d341ba0e0937cdc91a7e0af6965347b05c2cdab
Aravindh <32878238+s-aravindh@users.noreply.github.com>
2023-01-05 21:52:06 +0800

    aws module init

---
+++ multiocr/multiocr/ocr_aws_textract.py
@@ -0,0 +1,12 @@
+from base_class import OCR
+
+class AwsTextractOCR(OCR):
+
+    def image_to_str(self):
+        pass
+
+    def image_to_data(self):
+        pass
+
+    def image_to_json(self):
+        pass
