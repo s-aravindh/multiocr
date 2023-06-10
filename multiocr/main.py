57cf1a552c406c0aff728d0fa4324e614d70d539
Aravindh <32878238+s-aravindh@users.noreply.github.com>
2023-06-10 17:06:39 +0800

    multiocr interface added

--- multiocr/main.py
+++ multiocr/main.py
@@ -1,3 +1,52 @@
-from multiocr.ocr_aws_textract import AwsTextractOCR
-from multiocr.ocr_paddle import PaddleOCR
-from multiocr.ocr_tesseract import TesseractOCR
+from multiocr.pipelines import AwsTextractOcr
+from multiocr.pipelines import TesseractOcr
+from multiocr.pipelines import PaddleOcr
+from base_class import OCR
+from typing import Union
+
+ENGINE_DICT = {"aws_textract":AwsTextractOcr, 
+               "tesseract":TesseractOcr, 
+               "paddle_ocr":PaddleOcr}
+avail_ocr_backends = ["tesseract", "aws_textract", "paddle_ocr"]
+
+class OcrEngineSelectionError(Exception):
+    def __init__(self, msg:str) -> None:
+        super().__init__()
+        pass
+
+
+class OcrEngine(OCR):
+    def __init__(self, engine:str, config:Union[dict, None]=None) -> None:
+        self.config = config
+        self.engine = ENGINE_DICT[engine](self.config) if engine in avail_ocr_backends else None
+        if self.engine is None:
+            raise OcrEngineSelectionError(f"only these ocr backends are available : {avail_ocr_backends}")
+    
+    def text_extraction(self, image_file):
+        text_dict = self.engine.text_extraction(image_file)
+        return text_dict
+
+    def text_extraction_to_json(self, text_dict):
+        json_dict = self.engine.text_extraction_to_json(text_dict)
+        return json_dict
+
+    def text_extraction_to_df(self, text_dict):
+        df = self.engine.text_extraction_to_df(text_dict)
+        return df
+
+    def extract_plain_text(self, text_dict):
+        plain_text = self.engine.extract_plain_text(text_dict)
+        return plain_text
+   
+if __name__ == "__main__":
+    image_file = "/Users/aravindh/Documents/GitHub/multiocr/tests/data/test-european.jpg"
+    config = {
+        "lang": "eng",
+        "config" : "--psm 6"
+    }
+    engine = OcrEngine("tesseract", config)
+    text_dict = engine.text_extraction(image_file)
+    json = engine.text_extraction_to_json(text_dict)
+    df = engine.text_extraction_to_df(text_dict)
+    plain_text = engine.extract_plain_text(text_dict)
+    print()
